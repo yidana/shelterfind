6d2e1fa4f8425f0130a5365d129b298f6884764d
ian akoto <iancecilakoto@gmail.com>
2018-09-17 13:23:46 -1200

    https://github.com/ian0549/shelterfind.git

---
+++ app/src/main/java/findhome/com/example/android/findhomeb/workers/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package findhome.com.example.android.findhomeb.workers
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService:FirebaseMessagingService(){
+
+
+
+
+
+
+}
